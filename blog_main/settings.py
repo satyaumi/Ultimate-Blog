28e510936077d571a8204412c73d517007981a40
satyaumi <satyamlkinf@gmail.com>
2025-12-29 10:26:15 +0530

    secure the site

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -147,8 +147,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'satyamlkinf@gmail.com'
-EMAIL_HOST_PASSWORD = 'exhk mezg pfsm scxy'
+EMAIL_HOST_USER = os.environ.get('satyamlkinf@gmail.com')
+EMAIL_HOST_PASSWORD =os.environ.get('exhk mezg pfsm scxy')
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
